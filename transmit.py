671655d663e6a9c9c387b980fb0555163463d34e
Adam Olech <me@aole.ch>
2025-01-14 02:12:18 +0100

    Clean up transmit.py

--- transmit.py
+++ transmit.py
@@ -13,12 +13,16 @@ def bin_list_to_string(message_lst):
     for chunk in __chunks(message_lst, 8):
         s.append(int("".join(map(str, chunk)), 2))
 
-    print(repr(s))
-
     return s
 
+def states_to_bin(states_lst):
+    return "".join(
+            ["01" if x == "1" else "00" if x == "0" else "10" if x == "f" else None for x in states_lst]
+            )
+
+def states_to_message(states_lst):
+    return bin_list_to_string(states_to_bin(states_lst))
+
 with serial.Serial(sys.argv[1], 9600, timeout=1) as ser:
-    #ser.write(bin_list_to_string("101010101010101000000001"))
-    ser.write(bin_list_to_string("101010101010101000000011"))
-    ser.flush()
+    ser.write(states_to_message(sys.argv[2]))
     ser.close()
