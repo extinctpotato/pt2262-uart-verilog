9dc08a130b508c3c28105cd7c94156dac968086d
Adam Olech <me@aole.ch>
2025-01-23 00:52:23 +0100

    Clean-up transmit.py and add mqtt mode

--- transmit.py
+++ transmit.py
@@ -1,6 +1,10 @@
 import serial
 import sys
 import time
+import argparse
+import pathlib
+import paho.mqtt.subscribe as paho_subscribe
+from typing import Callable, Any
 
 def bin_list_to_string(message_lst):
     def __chunks(lst, n):
@@ -20,9 +24,33 @@ def states_to_bin(states_lst):
             ["01" if x == "1" else "00" if x == "0" else "10" if x == "f" else None for x in states_lst]
             )
 
-def states_to_message(states_lst):
-    return bin_list_to_string(states_to_bin(states_lst))
+def tx_states(serial_port, states):
+    msg = bin_list_to_string(states_to_bin(states))
+    with serial.Serial(serial_port, 9600) as ser:
+        ser.write(msg)
 
-with serial.Serial(sys.argv[1], 9600, timeout=1) as ser:
-    ser.write(states_to_message(sys.argv[2]))
-    ser.close()
+def on_mqtt_message(serial_port) -> Callable[[Any], None]:
+    def __callback(client, _, msg):
+        tx_states(serial_port, msg.payload.decode())
+
+    return __callback
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='pt2262_verilog_tx')
+    parser.add_argument('-d', '--device',
+                        help='path to the TTY character device',
+                        default='/dev/ttyUSB0'
+                        )
+    parser.add_argument('-b', '--broker', type=str, help='mqtt broker hostname', default='localhost')
+    parser.add_argument('-t', '--topic', type=str, help='mqtt topic to subscribe to', default='pt2262_verilog')
+    subparsers = parser.add_subparsers(required=True)
+
+    mqtt = subparsers.add_parser('mqtt')
+    mqtt.set_defaults(func=lambda a: paho_subscribe.callback(on_mqtt_message(a.device) , a.topic, hostname=a.broker))
+
+    tx = subparsers.add_parser('tx')
+    tx.add_argument('msg', type=str, help='(e.g. ffffffff0001)')
+    tx.set_defaults(func=lambda a: tx_states(a.device, a.msg))
+
+    args = parser.parse_args()
+    args.func(args)
